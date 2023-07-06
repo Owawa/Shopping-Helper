1eb04e8cbb8c9c621872b7136ad1fe642e5b72aa
Owawa <makotograffitti@gmail.com>
2023-07-06 22:19:44 +0900

    add validate option to api, button event listener

--- public/stock.js
+++ public/stock.js
@@ -1,7 +1,7 @@
 const itemsDOM = document.querySelector(".items");
 
 //  get data from /api/v1/items
-const showitems = async () => {
+const showItems = async () => {
     try {
         const { data: items }  = await axios.get("/api/v1/items");
 
@@ -17,14 +17,10 @@ const showitems = async () => {
             
             return `<div class="single-item">
             <h5>${name} (threshold: ${threshold})</h5>
-            <div class="quantity ${(quantity < threshold) && "shortage"}" data-threshold=${threshold}>
-                <button type="button" class="quantity-btn minus" data-id="${_id}">
-                    -
-                </button>
-                ${quantity}
-                <button type="button" class="quantity-btn plus" data-id="${_id}">
-                    +
-                </button>
+            <div class="quantity-ctl ${(quantity <= threshold) && "shortage"}" data-id="${_id}" data-threshold=${threshold}>
+                <button type="button" class="quantity-btn minus">-</button>
+                <div><span class="quantity">${quantity}</span></div>
+                <button type="button" class="quantity-btn plus">+</button>
             </div>
         </div>`;
         }).join("");
@@ -33,8 +29,23 @@ const showitems = async () => {
         console.log(err);
     }
 }
+showItems();
 
 
+itemsDOM.addEventListener("click", async (event) => {
+    const elm = event.target;
 
+    if(elm.classList.contains("plus") || elm.classList.contains("minus")) {
+        const id = elm.parentElement.dataset.id;
+        const quantityElm = elm.parentElement.querySelector(".quantity");
+        const quantity = parseInt(quantityElm.innerText);
+        const delta = elm.classList.contains("plus") ? 1 : -1;
 
-showitems();
+        try {
+            await axios.patch(`/api/v1/items/${id}`, { quantity: quantity + delta });
+            showItems();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+});
